c656d5f224601295d3b344fab4c97653e1ddee8f
Jonas Sølvsteen <j08lue@gmail.com>
2018-01-04 10:44:48 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='s2_tci',
-    version='0.1',
+    version='0.2',
     description='Grab Sentinel 2 True Color Images from DHUS/SciHub',
     author='Jonas Solvsteen',
     author_email='josl@dhigroup.com',
